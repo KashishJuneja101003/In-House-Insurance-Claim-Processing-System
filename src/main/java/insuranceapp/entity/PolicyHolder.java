be8d66036799f706a8601563b3108bae078626d1
Kashish Juneja <VE00YN351@YMI.COM>
2026-02-19 07:26:30 +0530

    Policy Holder Entity Created

--- src/main/java/insuranceapp/entity/PolicyHolder.java
+++ src/main/java/insuranceapp/entity/PolicyHolder.java
@@ -1,5 +1,31 @@
 package insuranceapp.entity;
 
-public class PolicyHolder {
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
 
+@Entity
+@Data
+@AllArgsConstructor
+public class PolicyHolder {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "policyHolderId", nullable = false)
+	private String id;
+	
+	@Column(name = "name", nullable = false)
+	private String name;
+	
+	@Column(name = "email", nullable = false)
+	private String email;
+	
+	@Column(name = "contactNo", nullable = false)
+	private String contactNo;
+
+	@Column(name = "address", nullable = false)
+	private String Address;
 }
